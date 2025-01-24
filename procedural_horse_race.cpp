169160ac404288e2e66bf8af3302abdb38370a9e
EthanJade <erandreshak@bsu.edu>
2025-01-24 11:29:57 -0500

    Basic stuffs
    Uploaded the basic files needed for project

---
+++ procedural_horse_race.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <random>
+
+std::random_device rd;
+std:: uniform_int_distribution<int> dist(0, 1);
+
+void advance(int horseNum, int* horses);
+void printLane(int horseNum, int* horses);
+bool isWinner(int horseNum, int* horses);
+
+int main(){
+	printf("Welcome to the Races!\n");
+
+}
+
+void advance(){
+
+
+}
+
+void printLane(){
+
+
+}
+
+bool isWinner(){
+
+
+}	
+
